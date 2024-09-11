38ef744306bceec8c5b997a0608d48f0572f6442
gaby <gabrielmuise765@gmail.com>
2024-09-11 13:20:58 -0300

    Niveles mejorado

--- Main/Menu.gd
+++ Main/Menu.gd
@@ -6,7 +6,7 @@ func _on_Play_pressed():
 	# Limpiar las monedas al iniciar una nueva partida
 	CoinManager.reset_coins()
 	# Cambiar a la escena principal del juego
-	get_tree().change_scene("res://Worlds/World1.tscn")
+	get_tree().change_scene("res://Worlds/Easy/World1.tscn")
 
 func _on_Exit_pressed():
 	# Salir del juego
--- Worlds/level.gd
+++ Worlds/level.gd
@@ -3,4 +3,4 @@ extends Node2D
 export (int) var nextLevel
 
 func get_next_level():
-	return "res://Worlds/World" + str(nextLevel + 1) + ".tscn"
+	return "res://Worlds/Easy/World" + str(nextLevel + 1) + ".tscn"
