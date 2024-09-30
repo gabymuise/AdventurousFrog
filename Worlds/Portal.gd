efb7f3a4808eb7a247545b36eb86adee27ff4914
gaby <gabrielmuise765@gmail.com>
2024-09-30 15:10:25 -0300

    fin del nivel

--- Worlds/Portal.gd
+++ Worlds/Portal.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+export (String) var main_menu_scene_path = "res://Main/Menu.tscn"
+
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Players"):
 		var current_level = get_parent()
 		if current_level.has_method("get_next_level"):
 			var next_level_scene = current_level.get_next_level()
-			get_tree().change_scene(next_level_scene)
+			if next_level_scene != "" and ResourceLoader.exists(next_level_scene):
+				get_tree().change_scene(next_level_scene)
+			else:
+				print("No se encontró el siguiente nivel. Volviendo al menú principal.")
+				get_tree().change_scene(main_menu_scene_path)
